b3a6aa89798bafb8fb4421df7831e3fbfe6053ed
unclemeric <unclemeric@163.com>
2016-10-22 23:20:55 +0800

    修改使react项目可以跨域访问node项目

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
  */
 var App = {
     Config: {
-        api_site_prefix: 'http://localhost/'
-        // api_site_prefix: 'http://47065aba.nat123.net/'
+        api_site_prefix: 'http://localhost/',
+        // api_site_prefix: 'http://lai2.lai.com/',//http://lai2.lai.com/为另一个react的域名，设置这个之后可以让react项目跨域访问这个项目
     },
     API: {
         user: {
